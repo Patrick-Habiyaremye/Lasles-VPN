d4f44dc2aaae882125e5fd7ad3d0dfd9128424d2
patrick <habiyaremyepatrick.hp@gmaul.com>
2025-11-25 15:57:02 +0200

    Lasles VPN

---
+++ src/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home = () =>{
+    return(
+        <>
+          <div>
+            <div class="py-15">
+              <p><b>Want anything to be</b></p>
+              <p><b>easy with Lesles VPN.</b></p>
+            </div>
+            <div class="">
+                <img src="" class=""/>
+            </div>
+          </div>
+        </>
+    )
+}
+
+export default Home
